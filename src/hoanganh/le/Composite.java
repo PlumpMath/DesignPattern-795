4886601e0a66fe1788d6f6a463293ef9f71447e8
Lê Hoàng Anh <lehoanganh25991@gmail.com>
2015-09-11 15:13:36 +0700

    Composite commend

---
+++ src/hoanganh/le/Composite.java
@@ -0,0 +1,7 @@
+package hoanganh.le;
+
+public class Composite {
+public static void main(String[] args) {
+	System.out.println("Composite just accept MenuComponent as MenuItem with some bugs inside");
+}
+}
